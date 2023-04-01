b13ed148a72983b0324503900a513a7a6c5ec49f
Alejandra Urzola Mutis <jeimyurzola3@gmail.com>
2023-04-01 13:02:40 -0500

    Este es mi primer commit con git

---
+++ src/test/java/com/usa/primer/proyecto/PrimerProyectoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.usa.primer.proyecto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimerProyectoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
